cc1c61742f6ee0e1786cfef8dacb01cf61c99ef4
iSlavik <rikka879387@gmail.com>
2026-05-02 19:02:46 +0300

    Add FPS management methods to Menu class

--- src/gui/Menu.h
+++ src/gui/Menu.h
@@ -1,15 +1,16 @@
 #pragma once
 
+
 class Menu {
 public:
     static bool Setup();
     static void Shutdown();
     static void Render();
     static bool IsRunning();
-    
     static void SetWallhackEnabled(bool enabled);
     static bool IsWallhackEnabled();
-    
     static void SetVisible(bool visible);
     static bool IsVisible();
+    static void SetCurrentFPS(float fps);
+    static float GetCurrentFPS();
 };
