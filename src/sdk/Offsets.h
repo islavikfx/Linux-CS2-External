ae340aed47a8bdecd3ff1ec57796a5b2901cc2c9
iSlavik <rikka879387@gmail.com>
2026-05-08 11:23:24 +0300

    Updated WH Offset for last version

--- src/sdk/Offsets.h
+++ src/sdk/Offsets.h
@@ -2,30 +2,30 @@
 #include <cstdint>
 
 
-// # pgrep -f cs2; sudo cat /proc/X/maps | grep libclient.so; python3 -c 'print(hex(0x79f8de49c8e2 - 0x79f8dcc00000))'
+// # pgrep -f cs2; sudo cat /proc/X/maps | grep libclient.so; python3 -c 'print(hex(0x73a8d649b8f2 - 0x73a8d4c00000))'
 namespace Offsets {
-    inline uintptr_t xray = 0x189c8e2; // # may 2, 2026
+    inline uintptr_t xray = 0x189b8f2; // # may 8, 2026
 }
 
 
 std::string xrayasm = R"(
-Dump of assembler code from 0x79f8de49c8cf to 0x79f8de49c8f7:
-   0x000079f8de49c8cf:	mov    eax,DWORD PTR [rax]
-   0x000079f8de49c8d1:	test   rbx,rbx
-   0x000079f8de49c8d4:	sete   r12b
-   0x000079f8de49c8d8:	test   eax,eax
-   0x000079f8de49c8da:	sete   al
-   0x000079f8de49c8dd:	or     r12b,al
-   0x000079f8de49c8e0:	je     0x79f8de49c8f8
-   0x000079f8de49c8e2:	xor    eax,eax
-   0x000079f8de49c8e4:	add    rsp,0x38
-   0x000079f8de49c8e8:	pop    rbx
-   0x000079f8de49c8e9:	pop    r12
-   0x000079f8de49c8eb:	pop    r13
-   0x000079f8de49c8ed:	pop    r14
-   0x000079f8de49c8ef:	pop    r15
-   0x000079f8de49c8f1:	pop    rbp
-   0x000079f8de49c8f2:	ret
-   0x000079f8de49c8f3:	nop    DWORD PTR [rax+rax*1+0x0]
+Dump of assembler code from 0x73a8d649b8df to 0x73a8d649b907:
+   0x000073a8d649b8df:	mov    eax,DWORD PTR [rax]
+   0x000073a8d649b8e1:	test   rbx,rbx
+   0x000073a8d649b8e4:	sete   r12b
+   0x000073a8d649b8e8:	test   eax,eax
+   0x000073a8d649b8ea:	sete   al
+   0x000073a8d649b8ed:	or     r12b,al
+   0x000073a8d649b8f0:	je     0x73a8d649b908
+   0x000073a8d649b8f2:	xor    eax,eax
+   0x000073a8d649b8f4:	add    rsp,0x38
+   0x000073a8d649b8f8:	pop    rbx
+   0x000073a8d649b8f9:	pop    r12
+   0x000073a8d649b8fb:	pop    r13
+   0x000073a8d649b8fd:	pop    r14
+   0x000073a8d649b8ff:	pop    r15
+   0x000073a8d649b901:	pop    rbp
+   0x000073a8d649b902:	ret
+   0x000073a8d649b903:	nop    DWORD PTR [rax+rax*1+0x0]
 End of assembler dump.
 )";
