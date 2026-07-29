c7600705121af4326972757c1d8c4bd99ca6e8d6
iSlavik <rikka879387@gmail.com>
2026-07-29 17:09:14 +0300

    Updated Offsets.h for last version

--- src/sdk/Offsets.h
+++ src/sdk/Offsets.h
@@ -3,46 +3,47 @@
 
 
 // # unknown@unknown:~$ pgrep -f cs2
-// # 6177
-// # 6178
-// # 6220
-// # 6233
-// # 6234
-// # 6261
-// # 6266
-// # unknown@unknown:~$ sudo cat /proc/6266/maps | grep libclient.so
-// # 7192aca00000-7192ad663000 r--p 00000000 103:02 1966415                   /home/unknown/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/Counter-Strike Global Offensive/game/csgo/bin/linuxsteamrt64/libclient.so
-// # 7192ad663000-7192b0cac000 r-xp 00c62000 103:02 1966415                   /home/unknown/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/Counter-Strike Global Offensive/game/csgo/bin/linuxsteamrt64/libclient.so
-// # 7192b0cac000-7192b0f01000 r--p 042aa000 103:02 1966415                   /home/unknown/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/Counter-Strike Global Offensive/game/csgo/bin/linuxsteamrt64/libclient.so
-// # 7192b0f01000-7192b0fac000 rw-p 044fe000 103:02 1966415                   /home/unknown/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/Counter-Strike Global Offensive/game/csgo/bin/linuxsteamrt64/libclient.so
-// # unknown@unknown:~$ python3 -c 'print(hex(0x00007192ae3e45b2 - 0x7192aca00000))'
-// # 0x19e45b2
+// # 4733
+// # 4734
+// # 4780
+// # 4793
+// # 4794
+// # 4821
+// # 4826
+// # unknown@unknown:~$ sudo cat /proc/4826/maps | grep -e libclient.so
+// # 752820a00000-752821663000 r--p 00000000 103:02 2097195                   /home/unknown/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/Counter-Strike Global Offensive/game/csgo/bin/linuxsteamrt64/libclient.so
+// # 752821663000-752824cab000 r-xp 00c62000 103:02 2097195                   /home/unknown/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/Counter-Strike Global Offensive/game/csgo/bin/linuxsteamrt64/libclient.so
+// # 752824cab000-752824f00000 r--p 042a9000 103:02 2097195                   /home/unknown/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/Counter-Strike Global Offensive/game/csgo/bin/linuxsteamrt64/libclient.so
+// # 752824f00000-752824fac000 rw-p 044fd000 103:02 2097195                   /home/unknown/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/Counter-Strike Global Offensive/game/csgo/bin/linuxsteamrt64/libclient.so
+// # unknown@unknown:~$ python3 -c 'print(hex(0x00007528223e4132 - 0x752820a00000))'
+// # 0x19e4132
 // # unknown@unknown:~$ 
 
+
 namespace Offsets {
-    inline uintptr_t xray = 0x19e45b2; // # july 22, 2026
+    inline uintptr_t xray = 0x19e4132; // # july 29, 2026
 }
 
 
 std::string xrayasm = R"(
-Dump of assembler code from 0x7192ae3e459f to 0x7192ae3e45c7:
-   0x00007192ae3e459f:	mov    eax,DWORD PTR [rax]
-   0x00007192ae3e45a1:	test   rbx,rbx
-   0x00007192ae3e45a4:	sete   r12b
-   0x00007192ae3e45a8:	test   eax,eax
-   0x00007192ae3e45aa:	sete   al
-   0x00007192ae3e45ad:	or     r12b,al
-   0x00007192ae3e45b0:	je     0x7192ae3e45c8
-   0x00007192ae3e45b2:	xor    eax,eax
-   0x00007192ae3e45b4:	add    rsp,0x38
-   0x00007192ae3e45b8:	pop    rbx
-   0x00007192ae3e45b9:	pop    r12
-   0x00007192ae3e45bb:	pop    r13
-   0x00007192ae3e45bd:	pop    r14
-   0x00007192ae3e45bf:	pop    r15
-   0x00007192ae3e45c1:	pop    rbp
-   0x00007192ae3e45c2:	ret
-   0x00007192ae3e45c3:	nop    DWORD PTR [rax+rax*1+0x0]
+Dump of assembler code from 0x7528223e411f to 0x7528223e4147:
+   0x00007528223e411f:	mov    eax,DWORD PTR [rax]
+   0x00007528223e4121:	test   rbx,rbx
+   0x00007528223e4124:	sete   r12b
+   0x00007528223e4128:	test   eax,eax
+   0x00007528223e412a:	sete   al
+   0x00007528223e412d:	or     r12b,al
+   0x00007528223e4130:	je     0x7528223e4148
+   0x00007528223e4132:	xor    eax,eax
+   0x00007528223e4134:	add    rsp,0x38
+   0x00007528223e4138:	pop    rbx
+   0x00007528223e4139:	pop    r12
+   0x00007528223e413b:	pop    r13
+   0x00007528223e413d:	pop    r14
+   0x00007528223e413f:	pop    r15
+   0x00007528223e4141:	pop    rbp
+   0x00007528223e4142:	ret
+   0x00007528223e4143:	nop    DWORD PTR [rax+rax*1+0x0]
 End of assembler dump.
 )";
 
