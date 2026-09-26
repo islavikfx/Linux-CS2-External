90a50a39ce4106839ef936e3e7aeacda4289f5ac
iSlavik <williamssmith251@outlook.com>
2026-09-26 19:29:22 +0300

    Updated main.cpp for last version

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,7 @@ bool ValidateOffset() {
         return true;
     }
     
-    std::cout << "[+] Offset not matching (0x1a39244 + 0x0) - last update at 24 September 2026."
+    std::cout << "[+] Offset not matching (0x1a3a964 + 0x0) - last update at 26 September 2026."
     << " Maybe CS2 Updated. Check for update at GitHub page or try to restart game." << std::endl;
     return false;
 }
